7ad48c76581eabfb4844a2a78ece90327897070f
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-08-02 21:41:09 +0900

    (master) parse webhook

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use axum::{
+    extract::{Json, Path, Query},
     routing::{get, post},
     Router,
 };
-use log::info;
+use log::{debug, info, warn};
 
 #[tokio::main]
 async fn main() {
@@ -24,6 +25,34 @@ async fn health() -> &'static str {
     "health!"
 }
 
-async fn github_webhook() {
-    info!("github hook called!");
+#[derive(Debug)]
+struct Issue {
+    title: String,
+    body: String,
+    url: String,
+    node_id: String,
+}
+
+async fn github_webhook(Json(payload): Json<serde_json::Value>) {
+    // TODO return proper error to sender
+    let action: &str = payload["action"].as_str().expect("Malformed webhook");
+    info!("github hook called: {}", action);
+
+    match action {
+        "opened" => {
+            let issue_raw: &serde_json::Value = payload.get("issue").expect("No issue field");
+
+            let issue = Issue {
+                title: issue_raw["title"].as_str().unwrap().into(),
+                body: issue_raw["body"].as_str().unwrap().into(),
+                url: issue_raw["html_url"].as_str().unwrap().into(),
+                node_id: issue_raw["node_id"].as_str().unwrap().into(),
+            };
+
+            debug!("[webhook] issue opened {:?}", issue);
+        },
+        _ => {
+            warn!("Unhandled action type {}", action);
+        },
+    }
 }
